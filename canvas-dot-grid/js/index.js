d0b0f6ee7fa5f1010a02e8be7edf8fbb03b1d26d
unknown <shikharsuman59@gmail.com>
2018-10-14 14:45:37 -0500

    First commit

---
+++ canvas-dot-grid/js/index.js
@@ -0,0 +1,55 @@
+function getDocumentWidth() {
+  return Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+};
+
+function getDocumentHeight() {
+  return Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+};
+
+var canvas = document.getElementById('dotCanvas');
+var context = canvas.getContext('2d');
+
+var vw = getDocumentWidth(),
+    vh = getDocumentHeight();
+
+// resize the canvas to fill browser window dynamically
+
+
+
+// grid
+function drawGrid(){
+  var cellW = 10,
+      cellH = 10;
+
+  // vertical lines
+  for (var x = 0; x <= vw; x += cellW) {
+      context.moveTo(x, 0); // x, y
+      context.lineTo(x, vh);
+  }
+
+  // horizontal lines
+  for (var y = 0; y <= vh; y += cellH) {
+      context.moveTo(0, y); // x, y
+      context.lineTo(vw, y);
+  }
+
+  context.strokeStyle = "#cccccc";
+  context.stroke();
+}
+// drawGrid();
+
+// dots
+function drawDots() {
+  var r = 2,
+      cw = 50,
+      ch = 50;
+
+  for (var x = 20; x < vw; x+=cw) {
+    for (var y = 20; y < vh; y+=ch) {
+        context.font = "1px Calibri";
+        context.fillStyle = '#000000';
+        context.fillRect(x-r/2,y-r/2,r,r);
+      }
+  }
+}
+drawDots();
